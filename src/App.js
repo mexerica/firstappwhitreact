89e3ee0be43bbe8400535a9d54dd1040b791eba1
mexerica <produtivo63@gmail.com>
2021-08-09 20:26:08 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="vapor">
+        <div className="content">
+          <img className="palm" src="https://th.bing.com/th/id/R.afcafa7e13e00b9515a994b5acf127e4?rik=FXeG45gc1Yw4ZA&riu=http%3a%2f%2fi.imgur.com%2fEXUVL0W.gif&ehk=ZWSyVsCDpVju0feHuDrAfKXBmL4Jf3D9TCKZoPInK7k%3d&risl=&pid=ImgRaw&r=0" alt="Palm tree" />
+          <p>Final fantasy</p>
+          <h1 class="text-center">Veja mais <a href="https://pt.wikipedia.org/wiki/Final_Fantasy" target="_blank" alt="FF wikipedia">aqui</a> e <a href="https://github.com/mexerica" target="_blank" alt="Github">aqui</a></h1>
+          <img className="statue" src="https://i.gifer.com/origin/9b/9b7f5636e14079f70c84cd6ad99004b1_w200.gif" alt="A glitched statue to represent where human intellect has come from and where it's going" />
+          <img className="more" src= "https://i.pinimg.com/originals/e4/79/15/e479155922140abe2447dfdd359f8f19.gif" alt="more" />
+          <img className="spooky" src="https://orig00.deviantart.net/f025/f/2017/277/f/2/ffbe___cloud_strife_gif_2_by_zerolympiustrife-dbpjw5t.gif" alt="This spooky skeletal is to remind us that death is unescapable, lingering near at all times." />
+          <img className="checker" src="http://www.geocities.ws/finalfantasyfive/movepic/lancer.gif" alt="The checkerboard floor represents the game that is life." />
+          <img className="palmrev" src="https://th.bing.com/th/id/R.afcafa7e13e00b9515a994b5acf127e4?rik=FXeG45gc1Yw4ZA&riu=http%3a%2f%2fi.imgur.com%2fEXUVL0W.gif&ehk=ZWSyVsCDpVju0feHuDrAfKXBmL4Jf3D9TCKZoPInK7k%3d&risl=&pid=ImgRaw&r=0" alt="Palm trees to represent, almost laughably, the futility of nature being anything except aesthetic." />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
